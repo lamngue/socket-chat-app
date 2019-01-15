f637f39a262e2cd6240700af607ce3ad2c6127be
Lam Nguyen <lnguye16@gustavus.edu>
2019-01-15 09:11:44 +0700

    add new features

--- public/js/index.js
+++ public/js/index.js
@@ -14,4 +14,18 @@ socket.on('newEmail', function(email) {
 });
 socket.on('newMessage',(newMessage) => {
     console.log('newMessage', newMessage);
+    var li = $('<li></li>');
+    li.text(`${newMessage.from}: ${newMessage.text}`)
+    $('#messages').append(li);
+});
+
+
+$("#message-form").on('submit',function(e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from: 'User',
+        text: $('[name=message]').val()
+    },function(){
+
+    });
 });
